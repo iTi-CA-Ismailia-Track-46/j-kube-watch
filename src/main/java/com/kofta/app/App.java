0b0dbf7e1fe5b37eb60e43c8ced766ae234a8f9e
kofta999 <mostafaxxx555@gmail.com>
2026-04-07 21:24:00 +0200

    chore:  remove extra code from app

--- src/main/java/com/kofta/app/App.java
+++ src/main/java/com/kofta/app/App.java
@@ -1,15 +1,11 @@
 package com.kofta.app;
 
-import com.kofta.app.entities.Pod;
-
 /**
  * Hello world!
  */
 public class App {
 
     public static void main(String[] args) {
-        Pod pod = new Pod();
-        System.out.println(pod.name);
         System.out.println("Hello World!");
     }
 }
