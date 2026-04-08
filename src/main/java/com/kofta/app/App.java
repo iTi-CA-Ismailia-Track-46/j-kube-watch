2c3a775e08b9e73e59ff184fcf0a3885bcb13601
kofta999 <mostafaxxx555@gmail.com>
2026-04-08 15:41:53 +0200

    chore: test api client

--- src/main/java/com/kofta/app/App.java
+++ src/main/java/com/kofta/app/App.java
@@ -1,6 +1,9 @@
 package com.kofta.app;
 
 import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
 import io.kubernetes.client.util.Config;
 import java.io.IOException;
 
@@ -9,10 +12,19 @@ import java.io.IOException;
  */
 public class App {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ApiException {
         ApiClient apiClient = Config.defaultClient();
+        Configuration.setDefaultApiClient(apiClient);
 
-        System.out.println(apiClient.getBasePath());
-        System.out.println("Hello World!");
+        var api = new CoreV1Api();
+
+        var nodes = api
+            .listNode()
+            .labelSelector("beta.kubernetes.io/arch=amd63")
+            .execute();
+
+        for (var node : nodes.getItems()) {
+            System.out.println(node.getMetadata().getName());
+        }
     }
 }
