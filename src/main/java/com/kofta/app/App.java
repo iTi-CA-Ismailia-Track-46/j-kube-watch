d735f27de63c23ed9f383cbd068a36cdc35c18dc
kofta999 <mostafaxxx555@gmail.com>
2026-04-08 20:51:09 +0200

    refactor: use dotenv

--- src/main/java/com/kofta/app/App.java
+++ src/main/java/com/kofta/app/App.java
@@ -6,44 +6,42 @@ import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import io.fabric8.kubernetes.client.Watcher;
 import io.fabric8.kubernetes.client.WatcherException;
+import io.github.cdimascio.dotenv.Dotenv;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class App {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        var content = Files.readString(Path.of("D:\\Self Study\\End to End Project\\j-kube-watch\\config"));
+    public static void main(String[] args)
+        throws IOException, InterruptedException {
+        var dotenv = Dotenv.load();
+        var configPath = dotenv.get("CONFIG_FILE_PATH");
+
+        var content = Files.readString(Path.of(configPath));
         var config = Config.fromKubeconfig(content);
 
         KubernetesClient client = new KubernetesClientBuilder()
             .withConfig(config)
             .build();
 
-        client
-            .v1()
-            .events()
-            .watch(
-                new Events()
-            );
-
+        client.v1().events().watch(new Events());
     }
 }
 
-
 class Events implements Watcher<Event> {
+
     @Override
     public void eventReceived(Action action, Event resource) {
-        // System.out.print(resource.getMessage() + " ");
-        // System.out.print(resource.getInvolvedObject().getKind() + " ");
-        // System.out.print(resource.getInvolvedObject().getName() + " ");
-        // System.out.print(resource.getReason() + " ");
-        // System.out.print(resource.getEventTime() + " ");
-        // System.out.println(resource.getMetadata().getNamespace() + " ");
-        // System.out.println("*******************************************");
+        System.out.print(resource.getMessage() + " ");
+        System.out.print(resource.getInvolvedObject().getKind() + " ");
+        System.out.print(resource.getInvolvedObject().getName() + " ");
+        System.out.print(resource.getReason() + " ");
+        System.out.print(resource.getEventTime() + " ");
+        System.out.println(resource.getMetadata().getNamespace() + " ");
+        System.out.println("*******************************************");
     }
 
     @Override
-    public void onClose(WatcherException cause) {
-    }
+    public void onClose(WatcherException cause) {}
 }
