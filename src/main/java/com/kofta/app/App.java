e51d330e204624a0f2383471dc6e64e4600b2311
kofta999 <mostafaxxx555@gmail.com>
2026-04-08 15:27:17 +0200

    feat: add k8s api client

--- src/main/java/com/kofta/app/App.java
+++ src/main/java/com/kofta/app/App.java
@@ -1,11 +1,18 @@
 package com.kofta.app;
 
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.util.Config;
+import java.io.IOException;
+
 /**
  * Hello world!
  */
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ApiClient apiClient = Config.defaultClient();
+
+        System.out.println(apiClient.getBasePath());
         System.out.println("Hello World!");
     }
 }
